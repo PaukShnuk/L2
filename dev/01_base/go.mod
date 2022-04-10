3ccefefb755543a9817a435e04479cef279da9f9
PaukShnuk <kindeev_v@mail.ru>
2022-04-10 17:55:50 +0300

    dev1

---
+++ dev/01_base/go.mod
@@ -0,0 +1,9 @@
+module github.com/PaukShnuk/L2/dev/01_base
+
+go 1.17
+
+require (
+	github.com/beevik/ntp v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20220407224826-aac1ed45d8e3 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+)
--- dev/01_base/main.go
+++ dev/01_base/main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-	"https://github.com/beevik/ntp"
-	"time"
+	"github.com/beevik/ntp"
+	"log"
+	"os"
 )
 
 func main() {
-	var i uint8
-	if i > 10 {
-		return
+
+	if time, err := ntp.Time("0.beevik-ntp.pool.ntp.org"); err != nil {
+		w := log.New(os.Stderr, "TimeErr: ", 0)
+		w.Println(err)
+		os.Exit(1)
+	} else {
+		fmt.Println(time)
 	}
-	i++
-	options := ntp.QueryOptions{Timeout: 30 * time.Second, TTL: 5}
-	response, err := ntp.QueryWithOptions("0.beevik-ntp.pool.ntp.org", options)
-	time := time.Now().Add(response.ClockOffset)
-	fmt.Println(time)
 }
