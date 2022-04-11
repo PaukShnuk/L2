d62b87c1c0c0e4f7f428da30be092370f91f6250
PaukShnuk <kindeev_v@mail.ru>
2022-04-11 18:37:19 +0300

    done dev2

--- dev/02_unpack/main.go
+++ dev/02_unpack/main.go
@@ -6,57 +6,59 @@ import (
 	"unicode"
 )
 
-func charAppend(str []rune, value rune, c rune) []rune {
-	intValue, err := strconv.Atoi(string(value))
+func charAppend(str *[]rune, value rune, c rune) {
+	intValue, err := strconv.Atoi(string(value)) // преобразование руны в int
 	if err != nil {
 		fmt.Println("error:", err)
 	}
-	for intValue > 1 {
-		str = append(str, c)
+	for intValue > 1 { // добавление в строку символов по значению пришедшей руны -1
+		*str = append(*str, c)
 		intValue--
 	}
-	return str
 }
 
-func unpack(str string) (string, error) {
+func Unpack(str string) (string, error) {
 	var outputString []rune
-	var prevSymbolIsDigit = true
-	var screen bool
+	var prevSymbolIsDigit = true // флаг принадлежности последнего символа
+	var screen bool              // флаг экранирования
 	var symbol rune
 	inputRuneString := []rune(str)
 
 	for _, value := range inputRuneString {
 		if unicode.IsDigit(value) { // если символ - число
-			if prevSymbolIsDigit {
+			if prevSymbolIsDigit { // и предыдущий символ - число (также, если число - первый символ в строке)
 				return "", fmt.Errorf("wrond string")
 			}
-			prevSymbolIsDigit = true
-			if screen {
+			if screen { // если текущая цифра экранирована
 				symbol = value
-				charAppend(outputString, 1, symbol)
-				charAppend(outputString, value, symbol)
+				outputString = append(outputString, symbol) // записываем в буфер и выводим
 				screen = false
+			} else { // если цифра не экранирована
+				charAppend(&outputString, value, symbol) // прибывляем символ из буфера в финальную строку
+				prevSymbolIsDigit = true
 			}
 
-			charAppend(outputString, value, symbol)
-		}
-		if value == 92 { // если символ знак экранирвоания
+		} else if value == 92 { // если символ - знак экранирвоания
 			prevSymbolIsDigit = false
 			if screen { // экранирование уже включено
 				screen = false
 				prevSymbolIsDigit = false
 				symbol = value
+				outputString = append(outputString, symbol) // запись в буфер и добавление `\` как символа в финальную строку
+			} else {
+				screen = true
 			}
-			screen = true
+
 		} else { // если всё остальное
 			prevSymbolIsDigit = false
 			symbol = value
-			charAppend(outputString, 1, symbol)
+			screen = false
+			outputString = append(outputString, symbol) // добавление символа в финальную строку
 		}
 	}
 	return string(outputString), nil
 }
 
 func main() {
-	fmt.Println(unpack("/d1"))
+	fmt.Println(Unpack(`qwe\\45`))
 }
